a46ee22318e75f1c37f5ebe17a4decb7c29ea178
anthonyjlmorel <anthony.jlmorel@gmail.com>
2014-07-05 20:56:34 +0200

    Contacts

--- pages/contact.php
+++ pages/contact.php
@@ -1,3 +1,42 @@
 <div class="contact">
-contact
+<div class="row">
+	<div class="author-featurette">
+		<h2>Anthony</h2>
+		<div class="author-description">
+			Software Engineer
+		</div>
+		<div class="author-networks">
+			<ul>
+				<li>
+					<a href="javascript:window.open('http://fr.linkedin.com/pub/anthony-morel/26/8b3/368')" style="text-decoration:none;"><img src="<?php echo SITE_WEB_ADDR."/imgs/networks/linkedin_44x36.png"; ?>" alt="View Anthony's LinkedIn profile" style="vertical-align:middle" border="0"></a>
+				</li>
+				<li>
+					<a href="javascript:window.open('https://twitter.com/Anthonyjlmorel')" style="text-decoration:none;"><img src="<?php echo SITE_WEB_ADDR."/imgs/networks/twitter_36.png"; ?>" alt="View Anthony's Twitter profile" style="vertical-align:middle" border="0"></a>
+				</li>
+				<li>
+					<a href="javascript:window.open('https://plus.google.com/u/0/+AnthonyMorel')" style="text-decoration:none;"><img src="<?php echo SITE_WEB_ADDR."/imgs/networks/google_36.png"; ?>" alt="View Anthony's Google profile" style="vertical-align:middle" border="0"></a>
+				</li>
+			</ul>
+		</div>
+	</div>
+
+	<div class="author-featurette">
+		<h2>Chris</h2>
+		<div class="author-description">
+			Software Engineer
+		</div>
+		<div class="author-networks">
+			<ul>
+				<li>
+					<a href="javascript:window.open('http://fr.linkedin.com/pub/chris-mitel/23/774/69')" style="text-decoration:none;"><img src="<?php echo SITE_WEB_ADDR."/imgs/networks/linkedin_44x36.png"; ?>" alt="View Chris's LinkedIn profile" style="vertical-align:middle" border="0"></a>
+				</li>
+				<li>
+					<a href="javascript:window.open('https://plus.google.com/u/0/110874636852270077529/posts')" style="text-decoration:none;"><img src="<?php echo SITE_WEB_ADDR."/imgs/networks/google_36.png"; ?>" alt="View Chris's Twitter profile" style="vertical-align:middle" border="0"></a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</div>
+
+
 </div>
