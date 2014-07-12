9f62c1a2022e9d0bc33803a88e9c396c6246145a
anthonyjlmorel <anthony.jlmorel@gmail.com>
2014-07-12 15:36:40 +0200

    COntact Page

--- php-components/footer.php
+++ php-components/footer.php
@@ -1,4 +1,35 @@
-<footer class="text-right">
-	<!-- No need for footer right now -->
-	<!-- <p><a href="<?php echo SITE_WEB_ADDR; ?>"><?php echo SITE_NAME; ?></a></p> -->
+<footer>
+	<a name="about-anchor"></a>
+	<div class="content">
+		<div><h3>Touch W</h3></div>
+		<div class="authors">
+			<div>
+				<div>Anthony</div>
+				<div class="author-networks">
+					<ul>
+						<li>
+							<a href="javascript:window.open('http://fr.linkedin.com/pub/anthony-morel/26/8b3/368');void(0)" style="text-decoration:none;"><img src="<?php echo SITE_WEB_ADDR."/imgs/networks/linkedin_44x36.png"; ?>" alt="View Anthony's LinkedIn profile" style="vertical-align:middle" border="0"></a>
+						</li>
+						<li>
+							<a href="javascript:window.open('https://twitter.com/Anthonyjlmorel');void(0)" style="text-decoration:none;"><img src="<?php echo SITE_WEB_ADDR."/imgs/networks/twitter_36.png"; ?>" alt="View Anthony's Twitter profile" style="vertical-align:middle" border="0"></a>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div>
+				<div>Chris</div>
+				<div class="author-networks">
+					<ul>
+						<li>
+							<a href="javascript:window.open('http://fr.linkedin.com/pub/chris-mitel/23/774/69');void(0)" style="text-decoration:none;"><img src="<?php echo SITE_WEB_ADDR."/imgs/networks/linkedin_44x36.png"; ?>" alt="View Chris's LinkedIn profile" style="vertical-align:middle" border="0"></a>
+						</li>
+						<li>
+							<a href="javascript:window.open('https://plus.google.com/u/0/110874636852270077529/posts');void(0)" style="text-decoration:none;"><img src="<?php echo SITE_WEB_ADDR."/imgs/networks/google_36.png"; ?>" alt="View Chris's Twitter profile" style="vertical-align:middle" border="0"></a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+	
 </footer>
