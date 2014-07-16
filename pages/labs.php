8a86da37b10f8a90cd0d4fc53186d0a08e7ebf01
anthonyjlmorel <anthony.jlmorel@gmail.com>
2014-07-16 09:47:10 +0200

    Labs Style

--- pages/labs.php
+++ pages/labs.php
@@ -1,7 +1,7 @@
 <div class="labs">
 
 <?php
-	if( isset($GLOBALS['labs'])){
+	if( isset($GLOBALS['labs']) && count($GLOBALS['labs']) > 0){
 		$i=0;
 		$lastI = count($GLOBALS['labs']) - 1;
 		foreach($GLOBALS['labs'] as $labName=>$cfg){
@@ -9,8 +9,8 @@
 ?>
 	<div class="lab-area">
 		<img class="lab-area-img <?php echo $i % 2 === 0 ? 'pull-right' : 'pull-left'; ?>" src="<?php echo LABS_ADDR.'/'.$labName.'/excerpt-img.png' ?>" />
-		<a class="<?php echo $i % 2 === 0 ? 'align-left' : 'align-right'; ?>" href="<?php echo SITE_WEB_ADDR."/?page=labs&l=".$labName; ?>"><h2><?php echo $cfg['title']; ?></h2></a>
-		<p class="lab-description <?php echo $i % 2 === 0 ? 'align-left' : 'align-right'; ?>">
+		<a class="<?php echo $i % 2 === 0 ? 'align-left' : 'align-right'; ?>" href="<?php echo SITE_WEB_ADDR."/?page=labs&l=".$labName; ?>"><h3><?php echo $cfg['title']; ?></h3></a>
+		<p class="lab-description">
 			<?php require LABS_PATH."/".$labName."/".$cfg["excerpt"]; ?>
 		</p>
 		<div class="supported-browsers">
@@ -58,7 +58,10 @@
 	}else{
 	
 ?>
-		<p>Experiments are coming ...</p>
+	<div class="align-center">
+		<h2>Experiments are coming !</h2>
+	</div>
+		
 <?php
 	}
 ?>
