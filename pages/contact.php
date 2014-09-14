63e71f7f35864f3198459caadf43e59c350c26d4
anthonyjlmorel <anthony.jlmorel@gmail.com>
2014-09-14 14:06:02 +0200

    Footer links

--- pages/contact.php
+++ pages/contact.php
@@ -7,13 +7,13 @@
 		<div class="author-networks">
 			<ul>
 				<li>
-					<a href="javascript:window.open('http://fr.linkedin.com/pub/anthony-morel/26/8b3/368')" style="text-decoration:none;"><img src="<?php echo SITE_WEB_ADDR."/imgs/networks/linkedin_44x36.png"; ?>" alt="View Anthony's LinkedIn profile" style="vertical-align:middle" border="0"></a>
+					<a href="http://fr.linkedin.com/pub/anthony-morel/26/8b3/368" target="_blank"><img src="<?php echo SITE_WEB_ADDR."/imgs/networks/linkedin_44x36.png"; ?>" alt="View Anthony's LinkedIn profile" style="vertical-align:middle" border="0"></a>
 				</li>
 				<li>
-					<a href="javascript:window.open('https://twitter.com/Anthonyjlmorel')" style="text-decoration:none;"><img src="<?php echo SITE_WEB_ADDR."/imgs/networks/twitter_36.png"; ?>" alt="View Anthony's Twitter profile" style="vertical-align:middle" border="0"></a>
+					<a href="https://twitter.com/Anthonyjlmorel"  target="_blank"><img src="<?php echo SITE_WEB_ADDR."/imgs/networks/twitter_36.png"; ?>" alt="View Anthony's Twitter profile" style="vertical-align:middle" border="0"></a>
 				</li>
 				<li>
-					<a href="javascript:window.open('https://plus.google.com/u/0/+AnthonyMorel')" style="text-decoration:none;"><img src="<?php echo SITE_WEB_ADDR."/imgs/networks/google_36.png"; ?>" alt="View Anthony's Google profile" style="vertical-align:middle" border="0"></a>
+					<a href="https://plus.google.com/u/0/+AnthonyMorel" target="_blank"><img src="<?php echo SITE_WEB_ADDR."/imgs/networks/google_36.png"; ?>" alt="View Anthony's Google profile" style="vertical-align:middle" border="0"></a>
 				</li>
 			</ul>
 		</div>
@@ -27,10 +27,10 @@
 		<div class="author-networks">
 			<ul>
 				<li>
-					<a href="javascript:window.open('http://fr.linkedin.com/pub/chris-mitel/23/774/69')" style="text-decoration:none;"><img src="<?php echo SITE_WEB_ADDR."/imgs/networks/linkedin_44x36.png"; ?>" alt="View Chris's LinkedIn profile" style="vertical-align:middle" border="0"></a>
+					<a href="http://fr.linkedin.com/pub/chris-mitel/23/774/69" target="_blank"><img src="<?php echo SITE_WEB_ADDR."/imgs/networks/linkedin_44x36.png"; ?>" alt="View Chris's LinkedIn profile" style="vertical-align:middle" border="0"></a>
 				</li>
 				<li>
-					<a href="javascript:window.open('https://plus.google.com/u/0/110874636852270077529/posts')" style="text-decoration:none;"><img src="<?php echo SITE_WEB_ADDR."/imgs/networks/google_36.png"; ?>" alt="View Chris's Twitter profile" style="vertical-align:middle" border="0"></a>
+					<a href="https://plus.google.com/u/0/110874636852270077529/posts" target="_blank"><img src="<?php echo SITE_WEB_ADDR."/imgs/networks/google_36.png"; ?>" alt="View Chris's Twitter profile" style="vertical-align:middle" border="0"></a>
 				</li>
 			</ul>
 		</div>
--- php-components/footer.php
+++ php-components/footer.php
@@ -7,10 +7,10 @@
 				<div class="author-networks">
 					<ul>
 						<li>
-							<a href="javascript:window.open('http://fr.linkedin.com/pub/anthony-morel/26/8b3/368');void(0)" style="text-decoration:none;"><img src="<?php echo SITE_WEB_ADDR."/imgs/networks/linkedin_44x36.png"; ?>" alt="View Anthony's LinkedIn profile" style="vertical-align:middle" border="0"></a>
+							<a href="http://fr.linkedin.com/pub/anthony-morel/26/8b3/368" target="_blank"><img src="<?php echo SITE_WEB_ADDR."/imgs/networks/linkedin_44x36.png"; ?>" alt="View Anthony's LinkedIn profile" style="vertical-align:middle" border="0"></a>
 						</li>
 						<li>
-							<a href="javascript:window.open('https://twitter.com/Anthonyjlmorel');void(0)" style="text-decoration:none;"><img src="<?php echo SITE_WEB_ADDR."/imgs/networks/twitter_36.png"; ?>" alt="View Anthony's Twitter profile" style="vertical-align:middle" border="0"></a>
+							<a href="https://twitter.com/Anthonyjlmorel" target="_blank"><img src="<?php echo SITE_WEB_ADDR."/imgs/networks/twitter_36.png"; ?>" alt="View Anthony's Twitter profile" style="vertical-align:middle" border="0"></a>
 						</li>
 					</ul>
 				</div>
@@ -20,10 +20,10 @@
 				<div class="author-networks">
 					<ul>
 						<li>
-							<a href="javascript:window.open('http://fr.linkedin.com/pub/chris-mitel/23/774/69');void(0)" style="text-decoration:none;"><img src="<?php echo SITE_WEB_ADDR."/imgs/networks/linkedin_44x36.png"; ?>" alt="View Chris's LinkedIn profile" style="vertical-align:middle" border="0"></a>
+							<a href="http://fr.linkedin.com/pub/chris-mitel/23/774/69" target="_blank"><img src="<?php echo SITE_WEB_ADDR."/imgs/networks/linkedin_44x36.png"; ?>" alt="View Chris's LinkedIn profile" style="vertical-align:middle" border="0"></a>
 						</li>
 						<li>
-							<a href="javascript:window.open('https://plus.google.com/u/0/110874636852270077529/posts');void(0)" style="text-decoration:none;"><img src="<?php echo SITE_WEB_ADDR."/imgs/networks/google_36.png"; ?>" alt="View Chris's Twitter profile" style="vertical-align:middle" border="0"></a>
+							<a href="https://plus.google.com/u/0/110874636852270077529/posts" target="_blank"><img src="<?php echo SITE_WEB_ADDR."/imgs/networks/google_36.png"; ?>" alt="View Chris's Twitter profile" style="vertical-align:middle" border="0"></a>
 						</li>
 					</ul>
 				</div>
