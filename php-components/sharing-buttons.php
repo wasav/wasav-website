f0db1da20c17a14aef3a8cbc8e366b536554fdca
anthonyjlmorel <anthony.jlmorel@gmail.com>
2014-10-12 16:18:36 +0200

    Sharing buttons twitter

--- php-components/sharing-buttons.php
+++ php-components/sharing-buttons.php
@@ -3,7 +3,19 @@
 
 <div>
 
-<a href="https://twitter.com/share" class="twitter-share-button" data-dnt="true" data-count="none" data-via="Anthonyjlmorel">Tweet</a>
+<?php 
+	$twitterHashtag = "wasav-blog";
+	if(isset($active)){
+		if($active === 'labs'){
+			$twitterHashtag = "wasav-labs";
+		}
+	}
+	
+?>
+
+<!-- Twitter  -->
+<a href="https://twitter.com/share" class="twitter-share-button" data-dnt="true" data-count="none" data-hashtags="<?php echo $twitterHashtag; ?>">Tweet</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 
 <!-- Google+ -->
 <div class="g-plus" data-action="share" data-annotation="none"></div>
@@ -18,10 +30,8 @@
 
 <!-- End Google+ -->
 
+<!-- Linked In  -->
 <script type="IN/Share" data-url="<?php echo "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]"; ?>"></script>
-
-<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-
 <script src="//platform.linkedin.com/in.js" type="text/javascript">
   lang: en_US
 </script>
